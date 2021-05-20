d7888d61928631010e3cb4393526c83c57bdc78c
LucasLima021 <lucaslimabjflu@gmail.com>
2021-05-20 16:12:15 -0300

    primeiro index

---
+++ index.dart
@@ -0,0 +1,20 @@
+void main() {
+  print('Hello Dart');
+  print('Lucas Lima');
+
+  /*Tipos de variáveis em Dart (declaração com var)
+  var nome = 'Joao';
+  var numero = 20;
+  */
+
+  //tipos de variáveis
+  String novoNome = 'Fred';
+  String cpf = '999.999.999-00';
+
+  int ano = 1970;
+
+  double altura = 1.75;
+
+  bool vv = true;
+  bool vf = false;
+}
